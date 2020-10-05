1c9693f5e29f06a90ebd80c52a013b2154f350f2
David <davidelbar2@gmail.com>
2020-10-05 18:54:55 +0200

    Okkay

--- src/Album/Album.js
+++ src/Album/Album.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Album.css";
 
-
 function Album() {
   return (
     <div className="container-album">
+      <a
+        href="http://www.75esession.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        <div className="kamehouse"> </div>
+      </a>
+
     </div>
   );
 }
--- src/Nepal/Nepal.js
+++ src/Nepal/Nepal.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nepal.css";
 
+import { BsInfoCircleFill } from "react-icons/bs";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 function Nepal() {
+  const [grandMaster, setGrandMaster] = useState(false);
+
+  const reveal = () => {
+    const state = grandMaster;
+    setGrandMaster(!state);
+  };
+
   return (
     <div className="container-nepal">
-        <div className="kamehouse"/>
+      <div className="info" onClick={reveal}>
+        {grandMaster ? <AiFillCloseCircle /> : <BsInfoCircleFill />}
+      </div>
+      <div className="container-liens">
+        {grandMaster && (
+          <div className="try">
+            <div className="splinter" />
+            <div>
+              <ul>
+                <a
+                  href="https://www.youtube.com/channel/UCdWGxWejEXZrKMz8Br37Glg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <li className="yt"></li>
+                </a>
+                <a
+                  href="https://open.spotify.com/artist/1E8w172gkuT5htpwaIEsEl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <li className="spotify"></li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
