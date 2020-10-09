02d2f41271736a821556e761d9508368974fa1ed
David <davidelbar2@gmail.com>
2020-10-09 17:55:02 +0200

    bug fix

--- src/Home/Content/AdiosBahamas.js
+++ src/Home/Content/AdiosBahamas.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { render } from "react-dom";
 import { useTrail, animated } from "react-spring";
 import "./AdiosBahamas.css";
 
@@ -36,4 +35,3 @@ export default function AdiosBahamas() {
   );
 }
 
-render(<AdiosBahamas />, document.getElementById("root"));
