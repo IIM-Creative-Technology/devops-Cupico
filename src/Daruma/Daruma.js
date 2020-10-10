46ee8b62cb77fbfe72f08fd7d360073efa150ebd
David <davidelbar2@gmail.com>
2020-10-10 16:12:39 +0200

    try fix

--- src/Daruma/Daruma.js
+++ src/Daruma/Daruma.js
@@ -31,7 +31,64 @@ function Daruma() {
         <Plx className="cloud-2" parallaxData={cloud2}></Plx>
       </div>
       <Plx className="container-title" parallaxData={title}>
-       
+        <ul className="title-left">
+          <li>
+            Opening <br />
+            <span>オープニング</span>
+          </li>
+          <li>
+            Ennemies Pt. 2 <br /> <span>敵</span>
+          </li>
+          <li>
+            En face
+            <br /> <span>敵</span>
+          </li>
+          <li>
+            Trajectoire
+            <br />
+            <span>道</span>
+          </li>
+          <li>
+            Vibe
+            <br />
+            <span>雰囲気</span>
+          </li>
+          <li>
+            Lemonade
+            <br />
+            <span>レモネード</span>
+          </li>
+          <li>
+            Là-bas
+            <br />
+            <span>あそこ</span>
+          </li>
+          <li>
+            Sundance
+            <br />
+            <span>サンダンス</span>
+          </li>
+          <li>
+            Millionaire
+            <br />
+            <span>富豪</span>
+          </li>
+          <li>
+            Sans voir
+            <br />
+            <span>見ずに</span>
+          </li>
+          <li>
+            Crossfader
+            <br />
+            <span>クロスフェーダー</span>
+          </li>
+          <li>
+            Daruma
+            <br />
+            <span>だるま</span>
+          </li>
+        </ul>
       </Plx>
     </Plx>
   );
